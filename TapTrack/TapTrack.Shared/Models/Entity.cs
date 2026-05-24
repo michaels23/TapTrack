a8525626abb4ee96ed0be1eb9315608064756df4
Mike Steigerwald <Steiger@UMich.edu>
2026-05-24 13:38:00 -0500

    Model the tap track

---
+++ TapTrack/TapTrack.Shared/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapTrack.Shared.Models
+{
+    internal class Entity
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ TapTrack/TapTrack.Shared/Models/Tap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapTrack.Shared.Models
+{
+    internal class Tap : Entity
+    {
+        public required string Label { get; set; }
+        public required string Color { get; set; }
+    }
+}
